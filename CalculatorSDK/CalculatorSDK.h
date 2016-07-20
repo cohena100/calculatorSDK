cf915a908b9c763a29e489ccb8aa17aac1e0a73e
Avi Cohen <cohena100@gmail.com>
2016-07-20 10:21:39 +0300

    Initial Commit

---
+++ CalculatorSDK/CalculatorSDK.h
@@ -0,0 +1,19 @@
+//
+//  CalculatorSDK.h
+//  CalculatorSDK
+//
+//  Created by Avi Cohen on 20/7/16.
+//  Copyright © 2016 Avi Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CalculatorSDK.
+FOUNDATION_EXPORT double CalculatorSDKVersionNumber;
+
+//! Project version string for CalculatorSDK.
+FOUNDATION_EXPORT const unsigned char CalculatorSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CalculatorSDK/PublicHeader.h>
+
+
